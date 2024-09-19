b5cf5a47c9e299c70797f3861a3f910559523319
luhouyang <luhouyang@gmail.com>
2024-09-19 23:50:38 +0800

    finished

---
+++ SEPT_2024/LU_HOU_YANG/Main.java
@@ -0,0 +1,13 @@
+package SEPT_2024.LU_HOU_YANG;
+
+import java.lang.System;
+
+class Main{
+    public static void main(String args[]){  
+        // write your code below this comment
+
+        System.out.println("GDSC UTP");
+
+        // write your code above this comment
+    }  
+}
--- SEPT_2024/LU_HOU_YANG/main.py
+++ SEPT_2024/LU_HOU_YANG/main.py
@@ -1,7 +1,7 @@
 def main():
     # write your code below this comment
 
-    for i in range(101): print(f'{i} {"foo" if (i%2 == 0) else "bar"}')
+    print('GDSC UTP')
 
     # write your code above this comment
 
